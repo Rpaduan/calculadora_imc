b5b9adf503fd015f20ea88fd3ba1d4d546e02654
Rpaduan <renato_paduan@hotmail.com>
2026-05-28 15:19:16 -0300

    const Modal

--- script.js
+++ script.js
@@ -2,14 +2,43 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+// const modalWrapper = document.querySelector('.modal-wrapper')
+// const modalMessage = document.querySelector('.modal .tittle span')
+// const modalBtnClose = document.querySelector('.modal button.close')
 
+const Modal = {
 
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .tittle span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+
+}
 
 form.onsubmit = event => {
     event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
-    console.log(weight, height)
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
 }
 
+Modal.buttonClose.onclick = () => {
+    Modal.close()
+    inputWeight.value = ""
+    inputHeight.value = ""
+}
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
